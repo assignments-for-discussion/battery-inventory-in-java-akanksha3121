e62773ec2b32fdd219cca1a7270bd049a769d234
akanksha3121 <76805462+akanksha3121@users.noreply.github.com>
2022-09-02 18:52:23 +0530

    Update code to reflect changes commanded in function

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -1,27 +1,41 @@
 package bunchbycycles;
 
 public class Main {
-  static class CountsByUsage {
-    public int lowCount = 0;
-    public int mediumCount = 0;
-    public int highCount = 0;
-  };
+    static class CountsByUsage {
+        public int lowCount = 0;
+        public int mediumCount = 0;
+        public int highCount = 0;
+    }
 
-  static CountsByUsage countBatteriesByUsage(int[] cycles) {
-    CountsByUsage counts = new CountsByUsage();
-    return counts;
-  }
+    ;
 
-  static void testBucketingByNumberOfCycles() {
-    System.out.println("Counting batteries by usage cycles...\n");
-    CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
-    assert(counts.lowCount == 2);
-    assert(counts.mediumCount == 3);
-    assert(counts.highCount == 1);
-    System.out.println("Done counting :)\n");
-  }
+    static CountsByUsage countBatteriesByUsage(int[] cycles) {
+        CountsByUsage counts = new CountsByUsage();
 
-  public static void main(String[] args) {
-    testBucketingByNumberOfCycles();
-  }
+        for (int cycle : cycles
+        ) {
+            if (cycle >= 920) {
+                counts.highCount++;
+            } else if (cycle < 400) {
+                counts.lowCount++;
+            } else {
+                counts.mediumCount++;
+            }
+        }
+        
+        return counts;
+    }
+
+    static void testBucketingByNumberOfCycles() {
+        System.out.println("Counting batteries by usage cycles...\n");
+        CountsByUsage counts = countBatteriesByUsage(new int[]{100, 300, 500, 600, 900, 1000});
+        assert (counts.lowCount == 2);
+        assert (counts.mediumCount == 3);
+        assert (counts.highCount == 1);
+        System.out.println("Done counting :)\n");
+    }
+
+    public static void main(String[] args) {
+        testBucketingByNumberOfCycles();
+    }
 }
