6a3ba5ca34dee3694d6899c52fc9066c8d3b19d3
akanksha3121 <76805462+akanksha3121@users.noreply.github.com>
2022-09-02 19:03:58 +0530

    Make testcase more informative

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -12,8 +12,8 @@ public class Main {
     static CountsByUsage countBatteriesByUsage(int[] cycles) {
         CountsByUsage counts = new CountsByUsage();
 
-        
-//Running foreach on int[] cycles here cycle represent's a single value 
+
+//Running foreach on int[] cycles here cycle represent's a single value
         for (int cycle : cycles
         ) {
             if (cycle >= 920) {
@@ -24,16 +24,16 @@ public class Main {
                 counts.mediumCount++;
             }
         }
-        
+
         return counts;
     }
 
     static void testBucketingByNumberOfCycles() {
         System.out.println("Counting batteries by usage cycles...\n");
         CountsByUsage counts = countBatteriesByUsage(new int[]{100, 300, 500, 600, 900, 1000});
-        assert (counts.lowCount == 2);
-        assert (counts.mediumCount == 3);
-        assert (counts.highCount == 1);
+        assert (counts.lowCount == 2) : "LowCount isn't right";
+        assert (counts.mediumCount == 3) : "MidCount isn't right";
+        assert (counts.highCount == 1) : "HighCount isn't right";
         System.out.println("Done counting :)\n");
     }
 
